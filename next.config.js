21dd19160eb991dbe06be92c1a8c4a46480681ca
weblavoro087-jpg <weblavoro087@gmail.com>
2026-06-11 15:14:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  // 🚀 Evita che Vercel analizzi i moduli Node interni di Clerk nel middleware Edge
+  serverExternalPackages: ['@clerk/nextjs'],
 };
 
 module.exports = nextConfig;
